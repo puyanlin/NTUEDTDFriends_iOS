00c1ea165c8c871896ee804ffeb65db36d0d08f0
Patrick <patrick.lin@fuhu.com>
2015-01-14 14:33:57 +0800

    add warning

--- friends/AppDelegate.m
+++ friends/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <Parse/Parse.h>
 #import "GAI.h"
 
+#warning before run this project,you should assign PARSE ApplicationID and Client KEY
 #define PARSE_APPLICATIONID @""
 #define PARSE_CLIENTKEY @""
 
